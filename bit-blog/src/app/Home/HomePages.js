8534634a35f312895f53df8deb5319213436e870
BIT student <classroom@bgit.rs>
2018-06-08 11:42:08 +0200

    TASK-4

---
+++ bit-blog/src/app/Home/HomePages.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+class HomePages extends React.Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+      showButton: false,
+      users: [],
+    };
+  }
+  render () {
+    return (
+      <div className="container">
+
+        <h1>POSTS</h1>
+
+        <div className="titleBlog">
+          <a href=""><Link to="/post">Title 1</Link></a>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus voluptate modi veniam fugiat? Eligendi quam quis
+            aperiam praesentium similique........
+          </p>
+        </div>
+
+        <div className="titleBlog">
+          <a href="">Title 1</a>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus voluptate modi veniam fugiat? Eligendi quam quis
+            aperiam praesentium similique.......
+          </p>
+        </div>
+        <div className="titleBlog">
+          <a href="">Title 1</a>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus voluptate modi veniam fugiat? Eligendi quam quis
+            aperiam praesentium similique.......
+          </p>
+        </div>
+        <div className="titleBlog">
+          <a href="">Title 1</a>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus voluptate modi veniam fugiat? Eligendi quam quis
+            aperiam praesentium similique..........
+          </p>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default HomePages;
---
+++ bit-blog/src/app/HomeSinglePost/PostTitle.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import morePosts from './MorePosts';
+import MorePosts from './MorePosts';
+class PostTitle extends React.Component {
+  render () {
+    return (
+      <div className="container">
+
+        <h1>POSTS</h1>
+
+        <a href="">Author Name</a>
+
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita fugit dolores rem dolorem in dicta accusantium dignissimos
+          provident? Voluptates vitae necessitatibus consectetur autem veritatis quibusdam aperiam suscipit aspernatur illo molestiae!
+          Culpa obcaecati molestias eaque possimus modi? Eligendi, quibusdam. Unde qui assumenda aperiam placeat cupiditate at nam
+          ducimus natus dolorem quasi. Doloremque fugiat aspernatur omnis, aut nisi obcaecati at consectetur dolores. Odio veritatis
+          commodi labore, repudiandae, harum quia quae rem voluptatum sed quam molestias odit nostrum totam? Id, ut quibusdam distinctio
+          eius in quae quis fugiat laboriosam quisquam, cupiditate incidunt possimus? Modi porro earum illum accusantium. Ducimus
+          officia et officiis cumque corrupti provident excepturi non. Tenetur eaque, id, aliquam expedita distinctio praesentium
+          esse consequatur aut ipsa architecto quibusdam ab exercitationem temporibus!
+        </p>
+        <MorePosts />
+      </div>
+    );
+  }
+}
+
+export default PostTitle;
